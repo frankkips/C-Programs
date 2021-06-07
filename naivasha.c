830c6f62c278f9ea928bfd40c7ac5655be3edeaf
frankkips <franklinekiplagat1@gmail.com>
2021-06-07 09:55:09 +0300

    update

--- naivasha.c
+++ naivasha.c
@@ -3,7 +3,7 @@
 int main(){
     for (int i = 1; i <= 5; i++){
         for (int j = 1; j <= 5; j++){
-            printf("%d" , i * j);
+            printf("%d x %d = %d\t", i, j, i * j);
         }
     }
     return 0;
