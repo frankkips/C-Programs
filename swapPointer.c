ef7167476adb31bf369e80692d4fae25d4e8356e
frankkips <franklinekiplagat1@gmail.com>
2021-06-17 21:04:23 +0300

    r

--- swapPointer.c
+++ swapPointer.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-
+// Declare the swap function b4 the main function
 void swap(int *a,int *b);
 int main(){
     int num1,num2;
 
+    // Prompt the user to Enter the two numbers
     printf("Please Enter your first number: \n");
     scanf("%d" , &num1);
 
@@ -12,6 +13,7 @@ int main(){
 
     printf("The numbers before swap are: num1 = %d and num2 = %d\n" , num1 ,num2);
 
+    // Lets call the function with parameters being the addresses of the num1 and num2 variables
     swap(&num1,&num2);
 
     printf("The numbers after swap are: num1 = %d and num2 = %d\n" , num1 ,num2);
@@ -20,7 +22,7 @@ int main(){
 void swap(int *a, int *b){
     int t;
 
-    t = *a;
+    t = *a;     // The swap code
     *a = *b;
     *b = t;
 }
